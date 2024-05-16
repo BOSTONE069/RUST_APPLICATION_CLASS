b84ffbea65dd54232258801fbc281e594fb505e5
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-16 14:09:57 -0700

    Rust Application

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //Variables
+    let x = 5;
+
+    println!("The value of x is: {}", x);
+
+    let x = 6;
+
+    println!("The value of x is: {}", x);
+
+
+    println!("Hello, world!");
+}
