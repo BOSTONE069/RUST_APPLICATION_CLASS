2a019df839d6a7f9682ce74a950654afc8c91138
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-16 14:32:00 -0700

    Constant variables in the code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    //Variables
+    // Variables
     let x = 5;
 
     println!("The value of x is: {}", x);
@@ -7,6 +7,14 @@ fn main() {
     let x = 6;
 
     println!("The value of x is: {}", x);
+
+    //Constant variables
+    const MAX_POINTS: u32 = 100_000;
+
+    println!(
+        "The value of MAX_POINTS is: {}",
+        MAX_POINTS
+    );
 
 
     println!("Hello, world!");
