a0031f775de0ffe4829e0018ca5dd39b8e414a54
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-22 09:55:20 +0300

    Working with enums in the code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// use std::io;
+//use std::io;
+
+use std::fmt;
 
 mod fruits;
 use fruits::print_fruits;
@@ -8,7 +10,34 @@ use countries::print_ccountries;
 
 mod cities;
 use cities::city::cities_world;
+const AGE: i32 = 13;
+
+
 fn main() {
+
+    const AGE: i32 = 13;
+
+    enum STATUS {
+        ADULT,
+        CHILD,
+    }
+    
+    // Implement the Display trait for STATUS
+    impl fmt::Display for STATUS {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                STATUS::ADULT => write!(f, "ADULT"),
+                STATUS::CHILD => write!(f, "CHILD"),
+            }
+        }
+    }
+    let user_status = if AGE > 18 {
+        STATUS::ADULT
+    } else {
+        STATUS::CHILD
+    };
+
+    println!("The status of this person is {}", user_status);
 
     let input_1: &str = "23";
 
