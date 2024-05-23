24966b575e4ed8a53e7577376319dedec1f7288b
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-23 15:43:41 +0300

    Added comments for borrowing

--- src/borrowing.rs
+++ src/borrowing.rs
@@ -8,17 +8,18 @@ pub fn borrowing(){
     println!("The value of s2 is: {}", s2);  
 }
 
-/// The function `insert_to_db` in Rust takes a reference to a `String` as a parameter and returns a
-/// reference to the same `String`.
+/// The function `insert_to_db` in Rust appends "World" to a mutable string reference and returns the
+/// modified string reference.
 /// 
 /// Arguments:
 /// 
-/// * `par`: The function `insert_to_db` takes a reference to a `String` as a parameter named `par`. It
-/// then returns a reference to the same `String` that was passed as the parameter.
+/// * `par`: The parameter `par` is a mutable reference to a `String` that is passed to the
+/// `insert_to_db` function.
 /// 
 /// Returns:
 /// 
-/// The function `insert_to_db` is returning a reference to a `String` type.
+/// The function `insert_to_db` is returning a mutable reference to the modified `String` after
+/// appending "World" to it.
 fn insert_to_db(par: & mut String) -> &String {
     par.push_str("World");
     return par
