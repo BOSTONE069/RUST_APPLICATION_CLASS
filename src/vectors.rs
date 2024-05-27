4389b400014ccd44dfc8c01b8d761999e291aaa0
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-26 20:49:36 -0700

    Matching values in vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -2,13 +2,12 @@ pub fn vectors(){
     let my_vec = vec![1,2,3,4,5];
 
     let result_2 = my_vec.get(3);
-
-
-
-    match result_2 {
-        Some(number)=> {
-            println!("")
+     match result_2 {
+        Some(number) => {
+            println!("The value at index 3 is: {}", number);
+        },
+        None => {
+            println!("No value found at index 3");
         }
     }
-
 }
