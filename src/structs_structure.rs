9e08f7ad370cccd57a772eecd4475dccfd855ec7
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-28 03:50:12 -0700

    Structs for calculating the area of a rectangle

--- src/structs_structure.rs
+++ src/structs_structure.rs
@@ -1,12 +1,21 @@
-pub fn print_area(){
-    let width1 = 50;
-    let height1 = 30;
-
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 
 
-fn area(width:u32, height:u32) -> u32 {
-    width * height
+pub fn print_area(){
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
