b730a955ac84e7e2662e66c3402a91c82b20ffc8
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-29 04:42:06 -0700

    Borrowing and working with associated function in the implementations of the structs

--- src/methods.rs
+++ src/methods.rs
@@ -12,13 +12,42 @@ impl Rectangle{
     fn width(&self) -> bool{
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    //Associated Functions
+    fn square(size: u32) -> Self{
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 pub fn methods(){
+    //Working with associated functions
+    let sq = Rectangle::square(3);
+    println!("{:#?}", sq);
+
     let rect1 = Rectangle {
         width: 50,
         height: 70,
     };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can react1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can react1 hold rect3? {}", rect1.can_hold(&rect3));
 
     println!("The area of the reactagmle is {} square pixels.", rect1.area());
 
