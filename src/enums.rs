c7051010a7773008ee3550fa4dc71b76f6e875f8
BOSTONE069 <bostoneochieng69@gmail.com>
2024-06-04 04:55:52 -0700

    Enums implementation

--- src/enums.rs
+++ src/enums.rs
@@ -6,7 +6,45 @@ enum IpAddrKind{
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn process(&self) {
+        match self {
+            Message::Quit => {
+                println!("Quitting the program");
+            },
+            Message::Move {x, y} => {
+                println!("Moving to ({}, {})", x, y);
+            },
+            Message::Write(text) => {
+                println!("Writing: {}", text);
+            },
+            Message::ChangeColor(r, g, b) => {
+                println!("Changing color to RGB({}, {}, {})", r, g, b);
+            },
+        }
+    }
+}
+
 pub fn enumms(){
+
+    let quit_message = Message::Quit;
+    let move_message = Message::Move {x: 10, y: 20};
+    let write_message = Message::Write(String::from("Hello, Rust!"));
+    let color_message = Message::ChangeColor(255, 0, 0);
+
+    quit_message.process();
+    move_message.process();
+    write_message.process();
+    color_message.process();
+
+    
     const AGE: i32 = 13;
 
     enum STATUS {
