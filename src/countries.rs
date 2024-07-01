0a0689f295269d879fb116c4ef415a4c0c9cfe57
BOSTONE069 <bostoneochieng69@gmail.com>
2024-06-30 19:28:18 -0700

    Addeded comments in the code

--- src/countries.rs
+++ src/countries.rs
@@ -1,3 +1,4 @@
+/// The function `print_countries` in Rust prints out a message about countries in the world.
 pub fn print_ccountries (){
     println!("Countries in the wolrd");
 }
--- src/fruits.rs
+++ src/fruits.rs
@@ -1,3 +1,4 @@
+/// The function `print_fruits` in Rust prints out the message "Fruits.....".
 pub fn print_fruits(){
     println!("Fruits.....");
 }
