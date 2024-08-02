633ef000d67ab6f081b3971d8e9baa9f34e7d414
BOSTONE069 <bostoneochieng69@gmail.com>
2024-08-02 15:51:22 +0300

    Working with closures in the rust code

--- bostone/src/closures.rs
+++ bostone/src/closures.rs
@@ -1,3 +1,10 @@
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+
+
 pub fn test_closures() {
 
     let add = |x, y| {
@@ -11,6 +18,14 @@ pub fn test_closures() {
 
     print_result(93);
 
+    
+    let mut p1 = Person{first_name: "Bostone".to_string(), last_name: "Ochieng".to_string()};
+
+    let mut change_name = |new_first_name: &str| p1.first_name = new_first_name.to_string(); 
+
+    change_name("Rhemney");
+
+    println!("{}", p1.first_name);
 
 
 }
--- src/closures.rs
+++ src/closures.rs
@@ -1,7 +1,28 @@
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
 pub fn test_closures() {
 
-    let add = |x, y| x + y;
+    let add = |x, y| {
+        print!("x: {}, y: {}", x, y); 
+        x + y
+    };
 
-    add(-3, 8);
+    let results = add(-3, 8);
+
+    let print_result = || println!("The result is: {}", results);
+
+    print_result();
+
+    let mut p1 = Person{first_name: "Bostone".to_string(), last_name: "Ochieng".to_string()};
+
+    let mut change_name = |new_first_name: &str| p1.first_name = new_first_name.to_string(); 
+
+    change_name("Rhemney");
+
+    println!("{}", p1.first_name);
+    println!("{}", p1.last_name);
 
 }
