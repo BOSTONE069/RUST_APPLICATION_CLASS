d8083bf0adc7ec076ffe45562c484fa77df77e4b
BOSTONE069 <bostoneochieng69@gmail.com>
2024-08-15 14:58:43 +0300

    Working with vectors in data manipulation

--- bostone/src/myvec.rs
+++ bostone/src/myvec.rs
@@ -45,11 +45,29 @@ struct Car {
 pub fn test_vec_car() {
     let mut car_list: Vec<Car> = vec![];
 
-    for _ in 1..100u8 {
+    let mut car_lot2: Vec<Car> = vec![];
+
+    for _ in 1..=10u8 {
         car_list.push(Car{manufacturer:"Porsche".to_string(), model:"Panamera".to_string(), year: 2024});
     }
+
+    for _ in 1..=10u8 {
+        car_lot2.push(Car{manufacturer:"BMW".to_string(), model:"SUV".to_string(), year: 2023});
+    }
+
+
+    car_list.append(&mut car_lot2);
+
+    car_list.insert(0, Car{manufacturer:"RangeRover".to_string(), model:"Landorover".to_string(), year:2020});
+
+
+    car_list.remove(0);
 
     println!("{:?}", car_list);
     println!("{:?}", car_list.len());
     println!("{:?}", car_list.capacity());
+
+    println!("{:?}", car_lot2);
+    println!("{:?}", car_lot2.len());
+    println!("{:?}", car_lot2.capacity());
 }
